6ae7a4a364df4d91c0fee5ac43df39557d3cbe8c
sheluvdayzi <ziyadclombe.pro@gmail.com>
2023-06-01 17:05:39 +0200

    en cours

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,15 +37,21 @@ for(let i = 0; i < articlesPlats.length; i++){
     let h3Created = document.createElement("h3");
     let textCreated = document.createElement("p");
     let imgCreated = document.createElement("img");
+    let articleCreated = document.createElement("article");
+    let divImgContain = document.createElement("div");
     h3Created.className = 'titleCard';
     textCreated.className = 'textCard';
     imgCreated.className = 'imgCard';
+    articleCreated.className = 'containCard';
+    divImgContain.className = 'imgContain'
     imgCreated.setAttribute('src', articlesPlats[i].imgSrc);
     imgCreated.setAttribute('alt', articlesPlats[i].alt);
     h3Created.textContent = articlesPlats[i].title;
     textCreated.textContent = articlesPlats[i].text;
-    cartePlats.appendChild(h3Created);
-    cartePlats.appendChild(textCreated);
-    cartePlats.appendChild(imgCreated);
-    
+    cartePlats.appendChild(articleCreated);
+    articleCreated.appendChild(h3Created);
+    articleCreated.appendChild(textCreated);
+    // articleCreated.appendChild(imgCreated);
+    articleCreated.appendChild(divImgContain);
+    divImgContain.appendChild(imgCreated);
 }
